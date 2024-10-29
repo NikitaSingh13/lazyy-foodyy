6b84096c31cb274451487b9da793e01a3f3af4c8
sahil <143598850+sahil-n06@users.noreply.github.com>
2024-10-29 19:29:17 +0530

    backend updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
-export const  connectDB = async () =>{
-
-    await mongoose.connect('mongodb+srv://nikki:iloveyoumorethenyou@cluster0.1i7q8.mongodb.net//food-del').then(()=>console.log("DB Connected"));
-   
+export const connectDB = async () => {
+    await mongoose.connect('mongodb+srv://nikki:iloveyoumorethenyou@cluster0.1i7q8.mongodb.net/yourDatabaseName?retryWrites=true&w=majority&appName=Cluster0')
+        .then(() => console.log("DB Connected"))
+        .catch((error) => console.log("Connection error:", error));
 }
